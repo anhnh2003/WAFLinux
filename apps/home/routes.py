b705690365e9f99a6d17336748a410bcbf32a841
anhnh2003 <anh.nhcoder@gmail.com>
2024-12-23 20:11:56 +0700

    fix regex pattern in parse_log()

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -268,12 +268,20 @@ def view_log():
     return render_template('home/view_log.html', log_entries=log_entries)
 
 def parse_log_line(line):
-    # Example log line format:
-    # 2024-12-22T23:24:59.573555-05:00 kali kernel: OUTPUT LOG: IN= OUT=eth0 SRC=10.0.2.128 DST=10.0.2.1 LEN=40 TOS=0x10 PREC=0x00 TTL=64 ID=34687 DF PROTO=TCP SPT=22 DPT=55616 WINDOW=697 RES=0x00 ACK URGP=0
+    # Define the regex pattern
     pattern = re.compile(
-        r'(?P<timestamp>\S+) (?P<hostname>\S+) kernel: (?P<chain>\w+): IN=(?P<in>\S*) OUT=(?P<out>\S*) MAC=(?P<mac>\S*) SRC=(?P<src>\S*) DST=(?P<dst>\S*) LEN=(?P<len>\d+) TOS=(?P<tos>\S*) PREC=(?P<prec>\S*) TTL=(?P<ttl>\d+) ID=(?P<id>\d+) DF PROTO=(?P<proto>\S*) SPT=(?P<spt>\d+) DPT=(?P<dpt>\d+) WINDOW=(?P<window>\d+) RES=(?P<res>\S*) ACK URGP=(?P<urgp>\d+)'
+        r'(?P<timestamp>\S+)\s+(?P<hostname>\S+)\s+kernel:\s+(?P<chain>[A-Z]+)\s+LOG:\s+'
+        r'IN=(?P<in_interface>\S*)\s+OUT=(?P<out_interface>\S*)\s+SRC=(?P<src_ip>\S+)\s+'
+        r'DST=(?P<dst_ip>\S+)\s+LEN=(?P<length>\d+)\s+TOS=(?P<tos>\S+)\s+PREC=(?P<prec>\S+)\s+'
+        r'TTL=(?P<ttl>\d+)\s+ID=(?P<id>\d+)\s+(?P<flags>\S+)\s+PROTO=(?P<protocol>\S+)\s+'
+        r'SPT=(?P<src_port>\d+)\s+DPT=(?P<dst_port>\d+)\s+WINDOW=(?P<window>\d+)\s+'
+        r'RES=(?P<res>\S+)\s+(?P<detail>.+)?'
     )
+
+    # Match the line using the pattern
     match = pattern.match(line)
+
+    # If the pattern matches, return a dictionary of parsed fields
     if match:
         return match.groupdict()
     return {}
--- apps/home/test.py
+++ apps/home/test.py
@@ -1,10 +1,25 @@
 import re
+
 def parse_log_line(line):
-    # Example log line format:
-    # Dec 20 11:31:24 DESKTOP-0KC9F2L kernel: [11974.490683] INPUT: IN=lo OUT= MAC=00:00:00:00:00:00:00:00:00:00:00:00:08:00 SRC=127.0.0.1 DST=127.0.0.1 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=62923 DF PROTO=TCP SPT=49104 DPT=5000 WINDOW=65495 RES=0x00 SYN URGP=0
-    pattern = re.compile(r'(?P<timestamp>\w+ \d+ \d+:\d+:\d+) (?P<hostname>\S+) kernel: \[\d+\.\d+\] (?P<chain>\w+): IN=(?P<in>\S*) OUT=(?P<out>\S*) MAC=(?P<mac>\S*) SRC=(?P<src>\S*) DST=(?P<dst>\S*) LEN=(?P<len>\d+) TOS=(?P<tos>\S*) PREC=(?P<prec>\S*) TTL=(?P<ttl>\d+) ID=(?P<id>\d+) DF PROTO=(?P<proto>\S*) SPT=(?P<spt>\d+) DPT=(?P<dpt>\d+) WINDOW=(?P<window>\d+) RES=(?P<res>\S*) SYN URGP=(?P<urgp>\d+)' )
+    # Define the regex pattern
+    pattern = re.compile(
+        r'(?P<timestamp>\S+)\s+(?P<hostname>\S+)\s+kernel:\s+(?P<chain>[A-Z]+)\s+LOG:\s+'
+        r'IN=(?P<in_interface>\S*)\s+OUT=(?P<out_interface>\S*)\s+SRC=(?P<src_ip>\S+)\s+'
+        r'DST=(?P<dst_ip>\S+)\s+LEN=(?P<length>\d+)\s+TOS=(?P<tos>\S+)\s+PREC=(?P<prec>\S+)\s+'
+        r'TTL=(?P<ttl>\d+)\s+ID=(?P<id>\d+)\s+(?P<flags>\S+)\s+PROTO=(?P<protocol>\S+)\s+'
+        r'SPT=(?P<src_port>\d+)\s+DPT=(?P<dst_port>\d+)\s+WINDOW=(?P<window>\d+)\s+'
+        r'RES=(?P<res>\S+)\s+(?P<detail>.+)?'
+    )
+
+    # Match the line using the pattern
     match = pattern.match(line)
+
+    # If the pattern matches, return a dictionary of parsed fields
     if match:
         return match.groupdict()
     return {}
-print(parse_log_line("2024-12-22T23:24:59.573504-05:00 kali kernel: OUTPUT LOG: IN= OUT=eth0 SRC=10.0.2.128 DST=10.0.2.1 LEN=216 TOS=0x10 PREC=0x00 TTL=64 ID=34686 DF PROTO=TCP SPT=22 DPT=55616 WINDOW=697 RES=0x00 ACK PSH URGP=0"))
+
+# Example usage
+log_line = "2024-12-22T23:24:59.573504-05:00 kali kernel: OUTPUT LOG: IN= OUT=eth0 SRC=10.0.2.128 DST=10.0.2.1 LEN=216 TOS=0x10 PREC=0x00 TTL=64 ID=34686 DF PROTO=TCP SPT=22 DPT=55616 WINDOW=697 RES=0x00 ACK PSH URGP=0"
+parsed = parse_log_line(log_line)
+print(parsed)
